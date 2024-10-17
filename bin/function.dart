90fb62886004261f414cf06c9c3460a3ca003f0b
AmirmahdiNourkazemi <nourkazemi80@gmail.com>
2024-10-17 22:52:16 +0330

    chore: name parameter func

--- bin/function.dart
+++ bin/function.dart
@@ -3,3 +3,7 @@ int sub({required Set<int> set}) {
   set.forEach((element) => i += element);
   return i;
 }
+
+void printInfo({required String name, required String gender}) {
+  print("Hello $name your gender is $gender.");
+}
