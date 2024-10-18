e9313ee13d9f61f0367b6a45d71e8e0b34871cb2
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2024-10-18 10:08:09 +0330

    feat: read file added

--- bin/file.dart
+++ bin/file.dart
@@ -1,5 +1,12 @@
 import 'dart:io';
 
+void readFile() {
+  File file = File('test.csv');
+  var lines = file.readAsLinesSync();
+  // lines.followedBy(['salam' + ' ' + 'hadi']);
+  lines.forEach((element) => print(element));
+}
+
 void file() {
   File file = File('test.txt');
   print(file.lengthSync());
@@ -8,4 +15,23 @@ void file() {
   var path = file.absolute.path.replaceAll('test.txt', 'copy.txt');
   print(path);
   print(file.copySync(path));
+}
+
+void writeFile() {
+  File file = File('test.csv');
+  file.writeAsStringSync('Name,Phone\n', mode: FileMode.append);
+
+  for (var i = 0; i < 3; i++) {
+    stdout.write('Enter name ${i + 1}: ');
+    String? name = stdin.readLineSync();
+    stdout.write('Enter phone ${i + 1}: ');
+    String? phone = stdin.readLineSync();
+    file.writeAsStringSync('$name,$phone\n',
+        mode: FileMode.append, flush: true);
+  }
+}
+
+void deleteFile() {
+  File file = File('test.csv');
+  file.deleteSync();
 }
