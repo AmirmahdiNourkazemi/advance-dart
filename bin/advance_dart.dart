8bca083a85772deb331a553be4216e2d31b0a1bf
AmirmahdiNourkazemi <nourkazemi80@gmail.com>
2024-10-18 12:03:42 +0330

    chore:learnListFillRange

--- bin/advance_dart.dart
+++ bin/advance_dart.dart
@@ -4,5 +4,5 @@ void main(List<String> arguments) async {
   // await setAndList().then((value) => print(value));
   //  print(input());
   // Set<int> set = {1, 2, 3, 4, 5};
-  learnList();
+  learnListFillRange();
 }
--- bin/list.dart
+++ bin/list.dart
@@ -1,6 +1,12 @@
-void learnList() {
+void learnListReplaceRange() {
   List<int> list = [1, 2, 3, 4, 5];
   final replacements = [6, 7];
   list.replaceRange(0, list.length, [5, 6]);
+  print(list);
+}
+
+void learnListFillRange() {
+  List<dynamic> list = ['Ab', 1, "bc"];
+  list.fillRange(0, 1, "hello");
   print(list);
 }
